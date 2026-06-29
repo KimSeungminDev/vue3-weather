9615dd48592c7fbbf740b828b8187e8dc0542b8e
KimSeungminDev <kimseungmin.dev@gmail.com>
2026-06-29 10:20:13 +0900

    Html => Vue 옮기기 작업 및 사진 리소스 추가

---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,29 @@
+<template>
+	<footer class="footer">
+		<ul class="footer__list">
+			<li class="footer__item">
+				<RouterLink>
+					<span class="material-symbols-outlined footer_icons"> home </span>
+				</RouterLink>
+			</li>
+			<li>
+				<RouterLink>
+					<span class="material-symbols-outlined footer_icons">
+						stacked_line_chart
+					</span>
+				</RouterLink>
+			</li>
+			<li>
+				<RouterLink>
+					<span class="material-symbols-outlined footer_icons"> search </span>
+				</RouterLink>
+			</li>
+		</ul>
+	</footer>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style lang="scss" scoped></style>
