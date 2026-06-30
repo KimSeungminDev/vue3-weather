5adf740c418b12df72400ac8d989c8fa04c8eef4
KimSeungminDev <kimseungmin.dev@gmail.com>
2026-06-30 10:11:51 +0900

    Footer 네비게이션 누락 부분 추가 조치

--- web/src/components/TheFooter.vue
+++ web/src/components/TheFooter.vue
@@ -2,19 +2,19 @@
 	<footer class="footer">
 		<ul class="footer__list">
 			<li class="footer__item">
-				<RouterLink>
+				<RouterLink to="/">
 					<span class="material-symbols-outlined footer_icons"> home </span>
 				</RouterLink>
 			</li>
 			<li>
-				<RouterLink>
+				<RouterLink to="/forecast">
 					<span class="material-symbols-outlined footer_icons">
 						stacked_line_chart
 					</span>
 				</RouterLink>
 			</li>
 			<li>
-				<RouterLink>
+				<RouterLink to="/search">
 					<span class="material-symbols-outlined footer_icons"> search </span>
 				</RouterLink>
 			</li>
